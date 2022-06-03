ad68d5da8efce14de3f4f66cf861bdb5a704a4f6
GitHub Action <action@github.com>
2022-06-03 17:53:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/swordzi/yarnidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swordzi/yarnidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.swordzi.yarnidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swordzi.yarnidea.MyBundle
 
 class MyProjectService(project: Project) {
 
